70f50737c28a15ca00cdfc16229c7264b20dda7a
Serkan Korkut <serkan.korkut@thundra.io>
2023-02-09 01:35:25 +0300

    Added open source repo links

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,21 +1,41 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 class FooterComponent extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {}
-    }
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <div>
-                <footer className="footer">
-                    <span className="text-muted">Made by Bro's Jr with ❤️</span>
-                </footer>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <footer className="footer">
+          <span className="text-muted">Made by Bro's Jr with ❤️</span>
+          {/* <div className="text-muted">Contribute</div> */}
+          <div>
+            <span> Help us on</span>
+            <a
+              href="https://github.com/segocago/afet-yardim-fe"
+              rel="noreferrer"
+              target="_blank"
+            >
+              {" FE "}
+            </a>
+            <span> or on </span>
+            <a
+              href="https://github.com/segocago/afet-yardim-be"
+              rel="noreferrer"
+              target="_blank"
+            >
+              {" BE"}
+            </a>
+            <span className="">!</span>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
-export default FooterComponent
+export default FooterComponent;
