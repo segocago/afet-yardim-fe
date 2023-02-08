23e61d587b5fe22df365731f23216a8156cd6758
Nurettin Akyüz <akyuz.nurettin@gmail.com>
2023-02-08 20:16:55 +0300

    Add base components

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class FooterComponent extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {
-                 
-        }
+        this.state = {}
     }
 
     render() {
         return (
             <div>
-                <footer className = "footer">
-                    <span className="text-muted">All Rights Reserved 2020 @JavaGuides</span>
+                <footer className="footer">
+                    <span className="text-muted">All Rights Reserved 2023</span>
                 </footer>
             </div>
         )
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
-import SiteService from '../services/SiteService'
+import Map from "./Map";
+import {MenuItem, Select} from "@mui/material";
+import SiteService from "../services/SiteService";
 
 class MainPage extends Component {
     constructor(props) {
@@ -11,7 +13,7 @@ class MainPage extends Component {
         }
     }
 
-    onSelectCity = city => {
+    handleSelectCity = city => {
         this.setState({selectedCity: city});
     }
 
@@ -28,48 +30,21 @@ class MainPage extends Component {
     render() {
         return (
             <div>
-                <h2 className="text-center">Deprem Yardım Uygulaması</h2>
+                <Map></Map>
                 <br></br>
-                <div className="row">
-                    <table className="table table-striped table-bordered">
-
-                        <thead>
-                        <tr>
-                            <th> Employee First Name</th>
-                            <th> Employee Last Name</th>
-                            <th> Employee Email Id</th>
-                            <th> Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.state.employees.map(
-                                employee =>
-                                    <tr key={employee.id}>
-                                        <td> {employee.firstName} </td>
-                                        <td> {employee.lastName}</td>
-                                        <td> {employee.emailId}</td>
-                                        <td>
-                                            <button onClick={() => this.editEmployee(employee.id)}
-                                                    className="btn btn-info">Update
-                                            </button>
-                                            <button style={{marginLeft: "10px"}}
-                                                    onClick={() => this.deleteEmployee(employee.id)}
-                                                    className="btn btn-danger">Delete
-                                            </button>
-                                            <button style={{marginLeft: "10px"}}
-                                                    onClick={() => this.viewEmployee(employee.id)}
-                                                    className="btn btn-info">View
-                                            </button>
-                                        </td>
-                                    </tr>
-                            )
-                        }
-                        </tbody>
-                    </table>
-
+                <div>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={this.state.selectedCity}
+                        label="City"
+                        onChange={this.handleSelectCity}
+                    >
+                        <MenuItem value={"Ankara"}>Ankara</MenuItem>
+                        <MenuItem value={"İstanbul"}>İstanbul</MenuItem>
+                        <MenuItem value={"İzmir"}>İzmir</MenuItem>
+                    </Select>
                 </div>
-
             </div>
         )
     }
