b9a21e12b28990b4091640138bd1bdae8125c13d
Deniz Alkışlar <deniz.alkislar@teampicnic.com>
2023-02-08 17:40:18 +0100

    Add map and its dependencies.

---
+++ src/map/map.js
@@ -0,0 +1,19 @@
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
+export default function Map() {
+    return (
+        <div>
+        <MapContainer center={[39.909442, 32.810491]} zoom={25} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[39.909442, 32.810491]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+        </div>
+    )
+}
+
