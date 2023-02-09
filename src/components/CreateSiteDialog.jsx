7700ae326fa7679b60d1dd79c091a73fe4dc5921
Serkan Korkut <serkan.korkut@thundra.io>
2023-02-10 00:52:48 +0300

    Added loading dialog for loading state of adding a site

--- src/components/CreateSiteDialog.jsx
+++ src/components/CreateSiteDialog.jsx
@@ -7,6 +7,8 @@ import {
   DialogContent,
   DialogTitle,
   TextField,
+  CircularProgress,
+  Backdrop,
 } from "@mui/material";
 import siteService from "../services/SiteService";
 import { CITIES } from "../constants/constants";
@@ -15,7 +17,7 @@ class CreateSiteDialog extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { formValues: {} };
+    this.state = { formValues: {}, isSiteProcessing: false };
   }
 
   handleCityInputChange = (event, newValue) => {
@@ -40,10 +42,13 @@ class CreateSiteDialog extends Component {
       alert("Şu alanların doldurulması zorunludur: " + invalidValues);
       return;
     }
-
+    this.setState({ isSiteProcessing: true });
     const response = await siteService.createSite(
       constructCreateSitePayload(formValues)
     );
+
+    this.setState({ isSiteProcessing: false });
+
     onNewSiteCreated(response.data);
     handleClose();
   };
@@ -88,120 +93,130 @@ class CreateSiteDialog extends Component {
   render() {
     const { handleInputChange } = this;
     return (
-      <Dialog open={this.props.open} onClose={this.props.handleClose}>
-        <DialogTitle>Yeni Yardım Noktası Yarat</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            name="name"
-            label="Yardım noktası ismi"
-            type="text"
-            fullWidth
-            variant="standard"
-            required
-            onChange={handleInputChange}
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="description"
-            name="description"
-            label="Açıklama"
-            type="text"
-            fullWidth
-            variant="standard"
-            onChange={handleInputChange}
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="contactInformation"
-            name="contactInformation"
-            label="İletişim Bilgileri"
-            type="text"
-            fullWidth
-            variant="standard"
-            onChange={handleInputChange}
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="organizer"
-            name="organizer"
-            label="Organize eden"
-            type="text"
-            fullWidth
-            variant="standard"
-            onChange={handleInputChange}
-          />
-          <Autocomplete
-            style={{ marginTop: 15 }}
-            disablePortal
-            options={CITIES}
-            renderInput={(params) => <TextField {...params} label="Şehir" />}
-            onChange={this.handleCityInputChange}
-            value={this.state.formValues.city}
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="district"
-            name="district"
-            label="İlçe"
-            type="text"
-            fullWidth
-            variant="standard"
-            onChange={handleInputChange}
-            required
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="additionalAddress"
-            name="additionalAddress"
-            label="Adres"
-            type="text"
-            fullWidth
-            variant="standard"
-            onChange={handleInputChange}
-            required
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="latitude"
-            name="latitude"
-            label="Enlem"
-            type="number"
-            fullWidth
-            variant="standard"
-            value={this.props.latitude}
-            required
-            disabled
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="longitude"
-            name="longitude"
-            label="Boylam"
-            type="number"
-            fullWidth
-            variant="standard"
-            value={this.props.longitude}
-            required
-            disabled
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => this.handleConfirmSiteCreation()}>
-            Yarat
-          </Button>
-          <Button onClick={() => this.props.handleClose()}>İptal</Button>
-        </DialogActions>
-      </Dialog>
+      <>
+        <Dialog open={this.props.open} onClose={this.props.handleClose}>
+          {
+            <Backdrop
+              sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+              open={this.state.isSiteProcessing}
+            >
+              <CircularProgress color="inherit" />
+            </Backdrop>
+          }
+          <DialogTitle>Yeni Yardım Noktası Yarat</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              name="name"
+              label="Yardım noktası ismi"
+              type="text"
+              fullWidth
+              variant="standard"
+              required
+              onChange={handleInputChange}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="description"
+              name="description"
+              label="Açıklama"
+              type="text"
+              fullWidth
+              variant="standard"
+              onChange={handleInputChange}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="contactInformation"
+              name="contactInformation"
+              label="İletişim Bilgileri"
+              type="text"
+              fullWidth
+              variant="standard"
+              onChange={handleInputChange}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="organizer"
+              name="organizer"
+              label="Organize eden"
+              type="text"
+              fullWidth
+              variant="standard"
+              onChange={handleInputChange}
+            />
+            <Autocomplete
+              style={{ marginTop: 15 }}
+              disablePortal
+              options={CITIES}
+              renderInput={(params) => <TextField {...params} label="Şehir" />}
+              onChange={this.handleCityInputChange}
+              value={this.state.formValues.city}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="district"
+              name="district"
+              label="İlçe"
+              type="text"
+              fullWidth
+              variant="standard"
+              onChange={handleInputChange}
+              required
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="additionalAddress"
+              name="additionalAddress"
+              label="Adres"
+              type="text"
+              fullWidth
+              variant="standard"
+              onChange={handleInputChange}
+              required
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="latitude"
+              name="latitude"
+              label="Enlem"
+              type="number"
+              fullWidth
+              variant="standard"
+              value={this.props.latitude}
+              required
+              disabled
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="longitude"
+              name="longitude"
+              label="Boylam"
+              type="number"
+              fullWidth
+              variant="standard"
+              value={this.props.longitude}
+              required
+              disabled
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => this.handleConfirmSiteCreation()}>
+              Yarat
+            </Button>
+            <Button onClick={() => this.props.handleClose()}>İptal</Button>
+          </DialogActions>
+        </Dialog>
+      </>
     );
   }
 }
