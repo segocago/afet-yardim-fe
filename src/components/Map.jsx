addfa061078318389c8b2eadfc84efa610874b03
Tolga Özgün <tolgaozgunn@gmail.com>
2023-02-10 02:01:33 +0300

    Remove ChangeView.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -49,7 +49,6 @@ const Map = ({handleCreateSiteDialogOpen, sites, center, addCommentToSite}) => {
       showOnlyVerified={showOnlyVerified}
       handleVerificationChange={handleVerificationChange}
     />
-      {/* <ChangeView center={center} /> */}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
