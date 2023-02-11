b4215ff08e78faeebd7c80477cb868b0d1c7f920
Deniz Alkışlar <deniz.alkislar@teampicnic.com>
2023-02-11 15:35:23 +0100

    Set max and min zoom levels. Refactor.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,10 @@
 import React, {useEffect, useState} from 'react'
 import {MapContainer, TileLayer} from "react-leaflet";
-import FilterBox from "./FilterBox";
 import SiteMarker from "./SiteMarker";
 
 const Map = ({sites, center, addCommentToSite, whenMapReady}) => {
 
   const [mapRef, setMapRef] = useState(null);
-
-  // Filters
-  const [showOnlyVerified, setShowOnlyVerified] = useState(false);
-  const handleVerificationChange = (value) => setShowOnlyVerified(value);
 
   useEffect(() => {
     if (center && !center.some(value => value === null || value === undefined) && mapRef) {
@@ -18,26 +13,13 @@ const Map = ({sites, center, addCommentToSite, whenMapReady}) => {
   }, [center])
 
   return (
-    <MapContainer ref={setMapRef} center={center} zoom={12} maxZoom={16} minZoom={11} scrollWheelZoom whenReady={whenMapReady}>
-    <FilterBox
-      showOnlyVerified={showOnlyVerified}
-      handleVerificationChange={handleVerificationChange}
-    />
+    <MapContainer ref={setMapRef} center={center} zoom={12} maxZoom={15} minZoom={9} scrollWheelZoom whenReady={whenMapReady}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {
-        sites.filter(
-          (site) => {
-            let valid_sites = site.location && site.location.latitude && site.location.longitude
-            
-            // Apply filters
-            if (showOnlyVerified) {
-              valid_sites = valid_sites && site.verified
-            }
-            return valid_sites
-          })
+        sites.filter(site => site.location && site.location.latitude && site.location.longitude)
           .map(site => {
             return (
               <SiteMarker site={site} addCommentToSite={addCommentToSite}></SiteMarker>
