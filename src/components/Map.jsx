e60c3bd905c78d227819eca1f70404d2f254752f
Selim Can Guler <selimcangler@gmail.com>
2023-02-11 16:46:04 +0300

    added clustering support for markers. minor fix for the positioning of the filtering label that gets on top of zoom icons for mobile devices

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react'
-import {MapContainer, Marker, Popup, TileLayer, Tooltip, useMap, useMapEvent} from "react-leaflet";
-import {Button, Comment, Form, Header, TextArea} from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, Tooltip, useMap, useMapEvent } from "react-leaflet";
+import MarkerClusterGroup from 'react-leaflet-cluster';
+import { Button, Comment, Form, Header, TextArea } from 'semantic-ui-react';
 import FilterBox from "./FilterBox";
 import SiteMarker from "./SiteMarker";
 
@@ -42,23 +43,32 @@ const Map = ({handleCreateSiteDialogOpen, sites, center, addCommentToSite,whenMa
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {
-        sites.filter(
-          (site) => {
-            let valid_sites = site.location && site.location.latitude && site.location.longitude
-            
-            // Apply filters
-            if (showOnlyVerified) {
-              valid_sites = valid_sites && site.verified
-            }
-            return valid_sites
-          })
-          .map(site => {
-            return (
-              <SiteMarker site={site} addCommentToSite={addCommentToSite}></SiteMarker>
-            )
-          })
-      }
+      <MarkerClusterGroup
+        chunkedLoading
+        showCoverageOnHover={true}
+        maxClusterRadius={100}
+        spiderfyOnMaxZoom={true}
+        disableClusteringAtZoom={12}
+        removeOutsideVisibleBounds
+      >
+        {
+          sites.filter(
+            (site) => {
+              let valid_sites = site.location && site.location.latitude && site.location.longitude
+              
+              // Apply filters
+              if (showOnlyVerified) {
+                valid_sites = valid_sites && site.verified
+              }
+              return valid_sites
+            })
+            .map(site => {
+              return (
+                <SiteMarker site={site} addCommentToSite={addCommentToSite}></SiteMarker>
+              )
+            })
+        }
+      </MarkerClusterGroup>
       <MyComponent></MyComponent>
     </MapContainer>
   );
